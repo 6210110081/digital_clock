c3557324e85b2f105471403af50039aa05cd99d2
6210110081 <6210110081@psu.ac.th>
2021-07-05 19:32:51 +0700

    class set

--- bin/digital_clock.dart
+++ bin/digital_clock.dart
@@ -1,6 +1,20 @@
 class Inc {}
 
-class Set {}
+class Chan {
+  int state;
+
+  Chan({this.state = 0});
+
+  void Set() {
+    if (state == 0) {
+      state = 1;
+    } else if (state == 1) {
+      state = 2;
+    } else if (state == 2) {
+      state = 0;
+    }
+  }
+}
 
 void main(List<String> arguments) {
   print('Hello world!');
